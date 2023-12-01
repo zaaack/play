067eeb80242600caf4e886fd084efac0ffd8a290
yangzhen <yangzhen@douban.com>
2023-12-01 16:42:32 +0800

    f

--- src/Anim/IterationCount/index.tsx
+++ src/Anim/IterationCount/index.tsx
@@ -9,19 +9,19 @@ export function AnimIterationCount(props: Props) {
   return (
     <div className='anim-iteration-count'>
         <div className='item'>
-            <Code className="label">animation-iterate-count: 0;</Code>
+            <Code className="label">animation-iteration-count: 0;</Code>
             <div className='box ' style={{animationIterationCount: 0}}></div>
         </div>
         <div className='item'>
-            <Code className="label">animation-iterate-count: 1.5;</Code>
+            <Code className="label">animation-iteration-count: 1.5;</Code>
             <div className='box ' style={{animationIterationCount: 1.5}}></div>
         </div>
         <div className='item'>
-            <Code className="label">animation-iterate-count: 2;</Code>
+            <Code className="label">animation-iteration-count: 2;</Code>
             <div className='box ' style={{animationIterationCount: 2}}></div>
         </div>
         <div className='item'>
-            <Code className="label">animation-iterate-count: infinite;</Code>
+            <Code className="label">animation-iteration-count: infinite;</Code>
             <div className='box ' style={{animationIterationCount: 'infinite'}}></div>
         </div>
     </div>
